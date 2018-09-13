0336c89e8a33b2dc8e46ca267429a057301cd834
wenhai <hai.wen@gmail.com>
2018-09-13 10:53:00 +0800

    隐藏接口实现

--- pkg/pkg.go
+++ pkg/pkg.go
@@ -10,3 +10,19 @@ type PhoneInterface interface {
 	Mprint()
 	Mycall()
 }
+
+type Mystruct struct {
+}
+
+func (ms Mystruct) Mprint() {
+
+}
+
+func (ms Mystruct) Mycall() {
+
+}
+
+func InitInterface() Mystruct {
+	aStruct := Mystruct{}
+	return aStruct
+}
